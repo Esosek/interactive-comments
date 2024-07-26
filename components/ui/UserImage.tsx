9326e1e96b2f74dad38ea5eb05955a79d14b88f3
Esosek <67476790+Esosek@users.noreply.github.com>
2024-07-26 16:33:02 +0200

    Added UserImage component

---
+++ components/ui/UserImage.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+import styles from "./styles/UserImage.module.css";
+
+type UserImageProps = {
+  username: string;
+  imagePath: string;
+  sizeInRem?: number;
+};
+
+export default function UserImage({
+  username,
+  imagePath,
+  sizeInRem = 2,
+}: UserImageProps) {
+  return (
+    <div
+      className={styles.wrapper}
+      style={{ height: `${sizeInRem}rem`, width: `${sizeInRem}rem` }}
+    >
+      <Image
+        src={imagePath}
+        alt={`Avatar of ${username}`}
+        fill
+        sizes={`${sizeInRem}rem`}
+      />
+    </div>
+  );
+}
