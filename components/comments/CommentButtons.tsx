f4b93e4ba4d21a511294d7abdeca8a3ce80986c5
Ales Zima <ales@cpex.cz>
2024-09-09 17:52:11 +0200

    WIP CommentButtons

--- components/comments/CommentButtons.tsx
+++ components/comments/CommentButtons.tsx
@@ -1,7 +1,17 @@
-import styles from "./styles/CommentButtons.module.css"
+import Image from 'next/image'
+
+import styles from './styles/CommentButtons.module.css'
+import iconReply from '@/public/images/icon-reply.svg'
 
 type CommentButtonsProps = {}
 
 export default function CommentButtons(props: CommentButtonsProps) {
-  return <button>Reply</button>
+  return (
+    <button className={styles.replyBtn}>
+      <div className={styles.iconWrapper}>
+        <Image src={iconReply.src} alt="reply icon" fill />
+      </div>
+      Reply
+    </button>
+  )
 }
