1beb3c1aeee58ba70c4d0b33ac849ac61b8ad3c3
Aleš Zima <zima.ales@seznam.cz>
2024-09-11 11:02:07 +0200

    Added "you" label to CommentHeader

--- components/comments/CommentHeader.tsx
+++ components/comments/CommentHeader.tsx
@@ -2,6 +2,7 @@ import { User } from '@/types/user'
 
 import styles from './styles/CommentHeader.module.css'
 import UserImage from '../common/UserImage'
+import { useUserStore } from '@/stores/userStore'
 
 type CommentHeaderProps = {
   user: User
@@ -12,10 +13,13 @@ export default function CommentHeader({
   user,
   createdAt: postedAt,
 }: CommentHeaderProps) {
+  const { loggedUser } = useUserStore()
+  const isUserOwned = loggedUser?.username === user.username
   return (
     <div className={styles.header}>
       <UserImage username={user.username} imagePath={user.image.webp} />
       <p className={styles.username}>{user.username}</p>
+      {isUserOwned && <p className={styles.userLabel}>you</p>}
       <p className={styles.postedText}>{postedAt}</p>
     </div>
   )
