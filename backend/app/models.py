4a0b3e496093c5a3fee0337f953ad4c32d0d8c80
Aleš Zima <zima.ales@seznam.cz>
2024-09-19 11:08:06 +0200

    Added parent_id and replying_to to Comment model

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,8 +5,10 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(255), nullable=False)
     createdAt = db.Column(db.Integer)
-    score = db.Column(db.Integer)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    # score = db.Column(db.Integer) will be moved to separate table
+    replying_to = db.Column(db.String(64), nullable=True)
+    user_id = db.Column(db.String, db.ForeignKey("user.id"), nullable=False)
+    parent_id = db.Column(db.String, db.ForeignKey("comment.id"), nullable=True)
 
 
 class User(db.Model):
--- backend/app/populate_db.py
+++ backend/app/populate_db.py
@@ -5,9 +5,13 @@ from .models import Comment, User
 def populate_db():
     user_01 = User(username="amyrobson", image="/images/avatars/image-amyrobson.webp")
     user_02 = User(username="maxblagun", image="/images/avatars/image-maxblagun.webp")
+    user_03 = User(
+        username="ramsesmiron", image="/images/avatars/image-ramsesmiron.webp"
+    )
 
     db.session.add(user_01)
     db.session.add(user_02)
+    db.session.add(user_03)
     db.session.commit()
 
     comment_01 = Comment(
@@ -26,4 +30,16 @@ def populate_db():
 
     db.session.add(comment_01)
     db.session.add(comment_02)
+    db.session.commit()
+
+    comment_03 = Comment(
+        content="If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+        createdAt=1725609436,
+        score=4,
+        user_id=user_03.id,
+        replying_to="maxblagun",
+        parent_id=comment_02.id,
+    )
+
+    db.session.add(comment_03)
     db.session.commit()
