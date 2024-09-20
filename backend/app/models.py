e7de652502abbb1b2c7810d75b102c4aec8c4302
Aleš Zima <zima.ales@seznam.cz>
2024-09-20 14:48:23 +0200

    Improved imports in models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,26 +1,27 @@
 from . import db
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
 
 
 class Comment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
-    replying_to = db.Column(db.String(64))
-    user_id = db.Column(db.String, db.ForeignKey("user.id"), nullable=False)
-    parent_id = db.Column(db.String, db.ForeignKey("comment.id"), nullable=True)
+    id = Column(Integer, primary_key=True)
+    content = Column(String(255), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    replying_to = Column(String(64))
+    user_id = Column(String, ForeignKey("user.id"), nullable=False)
+    parent_id = Column(String, ForeignKey("comment.id"), nullable=True)
     votes = db.relationship("Vote", backref="comment", lazy=True)
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), nullable=False)
-    image = db.Column(db.String(255), nullable=False)
+    id = Column(Integer, primary_key=True)
+    username = Column(String(64), nullable=False)
+    image = Column(String(255), nullable=False)
     comments = db.relationship("Comment", backref="user", lazy=True)
     votes = db.relationship("Vote", backref="user", lazy=True)
 
 
 class Vote(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    vote_type = db.Column(db.Boolean, nullable=False)
-    comment_id = db.Column(db.Integer, db.ForeignKey("comment.id"), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    id = Column(Integer, primary_key=True)
+    vote_type = Column(Boolean, nullable=False)
+    comment_id = Column(Integer, ForeignKey("comment.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
